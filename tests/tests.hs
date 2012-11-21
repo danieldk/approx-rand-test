baf140a639ff5a5049f96ac2a4a458c31fc0b3c6
Daniël de Kok <me@danieldk.eu>
2012-11-21 16:37:30 +0100

    Update the unit tests.

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,16 +1,18 @@
 module Main where
 
-import Control.Monad.Mersenne.Random (evalRandom)
-import System.Random.Mersenne.Pure64 (pureMT)
+import           Control.Monad.Error (runErrorT)
+import           Control.Monad.Mersenne.Random (evalRandom)
+import           Numeric.IEEE (IEEE, epsilon)
+import           System.Random.Mersenne.Pure64 (pureMT)
 import qualified Data.Vector.Unboxed as V
-import Statistics.Test.ApproxRand
-import Test.HUnit (assertEqual)
-import Test.Framework
-import Test.Framework.Providers.HUnit
+import           Statistics.Test.ApproxRand
+import           Test.HUnit (Assertion, assertBool, assertEqual)
+import           Test.Framework
+import           Test.Framework.Providers.HUnit
 
 tests :: Test
 tests = testGroup "Paired approximate randomization tests" $
-	concat [statTests, randomizationTests]
+  concat [statTests, randomizationTests]
 
 main :: IO ()
 main = defaultMain [ tests ]
@@ -22,8 +24,8 @@ statTests = [meanDifferenceTest]
 
 meanDifferenceTest :: Test
 meanDifferenceTest =
-	testEquality "mean difference robot competition"
-		(meanDifference cohenRobotsAlpha cohenRobotsBeta) 1.8
+  testIEEEEquality "mean difference robot competition"
+    1.8 $ meanDifference cohenRobotsAlpha cohenRobotsBeta
 
 -- Approximate andomization tests
 
@@ -32,18 +34,32 @@ randomizationTests = [pairApproxExactTestScores]
 
 pairApproxExactTestScores :: Test
 pairApproxExactTestScores =
-	testEquality  "number of extreme values robot competition"
-		(length $ filter (>= 1.8) scores) 21
-	where
-		scores = evalRandom
-			(approxRandPairScores meanDifference 1024
-				cohenRobotsAlpha cohenRobotsBeta) $
-			pureMT 42
+  testEquality  "number of extreme values robot competition"
+    (Right 21) $ V.length `fmap` V.filter (>= 1.8) `fmap` scores
+  where
+    test = runErrorT $
+      approxRandPairStats differenceMean 1024 cohenRobotsAlpha cohenRobotsBeta
+    scores = evalRandom test $ pureMT 42
 
 -- Helper functions
 
 testEquality :: (Show a, Eq a) => String -> a -> a -> Test
 testEquality msg a b = testCase msg $ assertEqual msg a b
+
+testIEEEEquality :: IEEE a => String -> a -> a -> Test
+testIEEEEquality msg a b = testCase msg $ assertEqualIEEE msg a b
+
+assertEqualIEEE :: IEEE a => String -> a -> a -> Assertion
+assertEqualIEEE msg a b = assertBool msg $ fracEq epsilon a b
+
+-- Suggested in The Floating Point Guide: http://floating-point-gui.de/
+fracEq :: (Fractional a, Ord a) => a -> a -> a -> Bool
+fracEq eps a b
+  | a == b     = True
+  | a * b == 0 = diff < (eps * eps)
+  | otherwise  = diff / (abs a + abs b) < eps
+  where diff = abs (a - b)
+   
 
 -- Example from Cohen, 1995
 cohenRobotsAlpha :: V.Vector Double
